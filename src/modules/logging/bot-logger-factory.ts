d3b147e6bc9683050f5ec24dadd2a1fe23a503e8
serjou1 <sergnik97@gmail.com>
2025-02-10 15:45:30 +0200

    add file logger

--- src/modules/logging/bot-logger-factory.ts
+++ src/modules/logging/bot-logger-factory.ts
@@ -3,3 +3,15 @@ import { BotLogger } from "./classes/bot-logger";
 export const createLogger = (...labels: string[]): BotLogger => {
     return new BotLogger(labels);
 };
+
+export const createFileLogger = (): BotLogger => {
+    const e = new Error();
+
+    const regex = /\((.*):(\d+):(\d+)\)$/
+    const match = regex.exec((e.stack as string).split("\n")[2]) as RegExpExecArray;
+
+    const paths = match[1].split('/').reverse();
+    const place = paths[0].startsWith('index') ? (paths[1] + '/' + paths[0]) : paths[0];
+
+    return createLogger(place);
+};
